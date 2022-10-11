b8ee6088b54fb5353db2941aef31325802d97263
noaweissfradin <noa.weiss@yahoo.fr>
2022-10-11 15:29:08 +0200

    add tests add all necessary functions from project 1

---
+++ src/test_search_tree.py
@@ -0,0 +1,55 @@
+# This directory will be checked with pytest. It will examine
+# all files that start with test_*.py and run all functions with
+# names that start with test_
+
+
+from elv import construct_tree
+
+def get_leafs(node,L):
+    
+    if node.is_leaf():
+        L.append(node.children)
+    
+    else: 
+        for letter in node.children:
+            child = node.get_child(letter)
+            if child.is_leaf():
+                L.append(child.children)
+            else:
+                get_leafs(child,L)
+
+def test_get_leafs():
+    '''Test if we can get all the leafs from a node'''
+
+    results = []
+    T = construct_tree('abab')
+    for branch in T:
+        L = []
+        get_leafs(branch, L)
+        results.append(sorted(L))
+    print(L)
+    expectations = [[0,1,2,3,4],[0],[1],[2],[3],[4],[0,2],[1,3]]
+    assert(sorted(results)==sorted(expectations))
+
+
+    results = []
+    T = construct_tree('abcd')
+    for branch in T:
+        L = []
+        get_leafs(branch, L)
+        results.append(sorted(L))
+    print(L)
+    expectations = [[0,1,2,3,4],[0],[1],[2],[3],[4]]
+    assert(sorted(results)==sorted(expectations))
+
+
+    results = []
+    T = construct_tree('aaaa')
+    for branch in T:
+        L = []
+        get_leafs(branch, L)
+        results.append(sorted(L))
+    print(L)
+    expectations = [[0,1,2,3,4],[0],[1],[2],[3],[4],[0,1,2,3],[0,1,2],[0,1]]
+    assert(sorted(results)==sorted(expectations))
+
