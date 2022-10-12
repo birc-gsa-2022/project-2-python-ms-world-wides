640662b0d8959adaf678debd32adb671eecc9a9e
Ch-Bun <charlotte.bunnenberg@gmx.de>
2022-10-12 17:35:34 +0200

    RUntime update

--- src/plot_runtime.py
+++ src/plot_runtime.py
@@ -43,15 +43,37 @@ def plot_runtime_construct(max_length, iterations):
     plt.legend(loc="upper left")
     plt.show()
 
-def plot_runtime_search(max_length, iterations):
-    #runtime = test_search(max_length, iterations)
-    pass
+def plot_runtime_search(x_length, p_max_length, iterations):
+    x = generate_rand_seq(x_length)
+    T = elv.construct_tree(x)
+    runtime = test_search(T, x, p_max_length, iterations)
+    plt.plot(runtime.keys(), runtime.values(), color="darkblue")#, label = f"with random sequence x")
+    #plt.plot(runtime_wc.keys(), runtime_wc.values(), color="red",label = f"with x = a^n" )
+    plt.xlabel('Size of pattern p [bp]')
+    plt.ylabel('Time [s]')
+    plt.title(f'Runtime of the algorithm for searching a pattern in a suffixtree')
+    #plt.legend(loc="upper left")
+    plt.show()
 
+def test_search(T, x, p_max_length, iterations):
+    runtime = {}
+    for i in range(5,p_max_length):
+        runtime_total = 0
+        #run multiple times to get the average
+        for j in range(iterations):
+            p = generate_rand_seq(i) 
+            # calc runtime
+            startTime = time.time()
+            elv.match(p, x, T)
+            executionTime = (time.time() - startTime)
+            runtime_total += executionTime
+        runtime[i] = runtime_total/iterations # add time to value in sequence length key
+    return runtime
+    
 def main():
     #try
-    plot_runtime_construct(1000, 10)
-    plot_runtime_search()
-
+    #plot_runtime_construct(100, 2)
+    plot_runtime_search(100,30,10)
 if __name__ == '__main__':
     main()
 
